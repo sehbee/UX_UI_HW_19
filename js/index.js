980e54133169171d930b2141f711a61f478d9993
Joe Lee <joejunelee98@gmail.com>
2020-12-05 20:27:49 -0800

    Created New Repo

---
+++ js/index.js
@@ -0,0 +1,32 @@
+$(".scrollbutton").click(function () {
+    // get the element on the page related to the button
+    var scrollToId = $(this).data("scroll");
+    var scrollToElement = document.getElementById(scrollToId);
+    // make the page scroll down to where you want
+    // ...
+});
+
+
+
+
+    (function changeWord(){
+        var words = [
+            'a film geek',
+            'a part-time musician',
+            'a coffee addict',
+            'a Korean-American',
+            'your friend',
+            'a perfectionist',
+            'a photographer',
+            'a lifelong Laker fan',
+            'a UX Designer'
+            ], i = 0;
+        setInterval(function changeWord(){
+            $('#whoami').fadeOut(function changeWord(){
+                $(this).html(words[i=(i+1)%words.length]).fadeIn();
+            });
+        }, 3000);
+          
+    })();
+
+    ScrollReveal().reveal('.hero-content');
